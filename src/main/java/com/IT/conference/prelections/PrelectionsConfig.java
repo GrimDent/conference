61eb07fdbe78dede52e20677392b8165093af2f1
GrimDent <hcse48@gmail.com>
2022-06-01 23:25:02 +0200

    Commit 6: Added prelections

--- src/main/java/com/IT/conference/prelections/PrelectionsConfig.java
+++ src/main/java/com/IT/conference/prelections/PrelectionsConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Math.floor;
-
 @Configuration
 public class PrelectionsConfig {
     @Bean
